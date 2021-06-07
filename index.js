1ee4aef04b37dbf887f0476384bc8f8dc7e0e41b
Dimitar <dimitarvelchev59@gmail.com>
2021-06-07 21:52:02 +0300

    Add service folder

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
 const express = require("express");
-const hbs = require("express-handlebars");
-const { about } = require("./controllers/about");
-const { catalog } = require("./controllers/catalog");
-const { create, post: createPost } = require("./controllers/create");
-const { details } = require("./controllers/details");
-const { edit, post: editPost } = require("./controllers/edit");
-const { notFound } = require("./controllers/notFound");
+const expressConfig = require("./config/express");
+const routesConfig = require("./config/routes");
 
-const { init: storage } = require("./models/storage");
+const { init: storage } = require("./services/storage");
+start();
 
 async function start() {
   const port = 3000;
   const app = express();
 
-  app.engine(
-    "hbs",
-    hbs({
-      extname: ".hbs",
-    })
-  );
-  app.set("view engine", "hbs"); //auto .hbs
-  app.use("/static", express.static("static"));
-  app.use(express.urlencoded({ extended: false }));
   app.use(await storage());
-
-  app.get("/", catalog);
-  app.get("/about", about);
-  app.get("/details/:id", details);
-  app.get("/create", create);
-  app.post("/create", createPost);
-  app.get("/edit/:id", edit);
-  app.post("/edit/:id", editPost);
-
-  app.all("*", notFound);
+  expressConfig(app);
+  routesConfig(app);
 
   app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 }
-
-start();
