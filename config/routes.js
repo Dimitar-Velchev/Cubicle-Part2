5e125fd8adb070bf9b46b79bbaf586c56670f907
Dimitar <dimitarvelchev59@gmail.com>
2021-06-09 16:30:06 +0300

    Add validation and accesory

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,9 @@ const { catalog } = require("../controllers/catalog");
 const { create, post: createPost } = require("../controllers/create");
 const { details } = require("../controllers/details");
 const { edit, post: editPost } = require("../controllers/edit");
+const { post: commentPost } = require("../controllers/comments");
 const { notFound } = require("../controllers/notFound");
+const { createAccessory } = require("../controllers/accessory");
 
 module.exports = (app) => {
   app.get("/", catalog);
@@ -13,6 +15,10 @@ module.exports = (app) => {
   app.post("/create", createPost);
   app.get("/edit/:id", edit);
   app.post("/edit/:id", editPost);
+
+  app.post("/comments/:cubeId/create", commentPost);
+
+  app.get('/accessory/create', createAccessory )
 
   app.all("*", notFound);
 };
